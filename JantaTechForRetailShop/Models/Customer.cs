f86394426063a7df594d13397272b1e092173378
pbakliwal <836pbakliwal@gmail.com>
2019-09-11 17:41:51 +0530

    SecondCommit

---
+++ JantaTechForRetailShop/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JantaTechForRetailShop.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name ="Mobile Number")]
+        [Required]
+        [RegularExpression(@"^([0]|\+91[\-\s]?)?[789]\d{9}$", ErrorMessage = "Entered Mobile No is not valid.")]
+        public string PhoneNo { get; set; }
+    }
+}
--- JantaTechForRetailShop/Models/IdentityModels.cs
+++ JantaTechForRetailShop/Models/IdentityModels.cs
@@ -21,6 +21,7 @@ namespace JantaTechForRetailShop.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
